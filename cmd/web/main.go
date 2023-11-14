247fadb56cd9e611f044385f9f09d4ee7cd48960
Mihail Tudos <mihailtudos@outlook.com>
2023-11-14 14:39:49 +0200

    added support for static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,12 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippets/view", snippetView)
 	mux.HandleFunc("/snippets/create", snippetCreate)
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
