50edbcaf8c4c25d86909d609e9d5366e22737ab2
Mihail Tudos <mihailtudos@outlook.com>
2023-11-17 22:51:41 +0200

    added a new modelView struct to pass data to the templates

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -74,7 +74,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", s)
+	data := templateData{Snippet: s}
+
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mihailtudos/snippetbox/internals/models"
+
+type templateData struct {
+	Snippet models.Snippet
+}
