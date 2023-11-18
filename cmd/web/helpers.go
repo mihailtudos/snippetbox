a94135e9825b272eef729e89634a95fc3f7398bd
Mihail Tudos <mihailtudos@outlook.com>
2023-11-18 18:23:21 +0200

    two-stage process of writing the templates

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log/slog"
 	"net/http"
@@ -33,7 +34,6 @@ func (app *application) notFound(w http.ResponseWriter) {
 
 // render helper renders the templates from the cache
 func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
-
 	// Retrieve the appropriate template set from the cache
 	ts, ok := app.templateCache[page]
 	if !ok {
@@ -42,11 +42,29 @@ func (app *application) render(w http.ResponseWriter, r *http.Request, status in
 		return
 	}
 
+	// If an error ocurred when parsing writing the templates
+	// the user would still get 200 OK and receive a broken (half generated page)
+	/*
+		To fix this we need to make the template render a two-stage process. First, we should make
+		a ‘trial’ render by writing the template into a buffer. If this fails, we can respond to the user
+		with an error message. But if it works, we can then write the contents of the buffer to our
+		http.ResponseWriter.
+	*/
+
+	buf := new(bytes.Buffer)
+
+	err := ts.ExecuteTemplate(buf, "base", data)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	// If the template is written to the buffer without any errors, we are safe
+	// to go ahead and write the HTTP status code to http.ResponseWriter.
 	// Write out the provided HTTP status code
 	w.WriteHeader(status)
 
 	// Execute the template set and write the response body
-	err := ts.ExecuteTemplate(w, "base", data)
+	buf.WriteTo(w)
 	if err != nil {
 		app.serverError(w, r, err)
 	}
